3ebace4eb0cc8731194633f18198d04cd6d5d5f3
Isha <ishagahlawat27@gmail.com>
2025-07-24 20:38:16 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -13,7 +13,7 @@ const app= express();
 const port=process.env.PORT ||3000;
 
 app.use(cors({
-    origin:"http://localhost:5173",
+    origin:"https://virtual-assistant-frontend-asf6.onrender.com",
     credentials:true
 }))
 
